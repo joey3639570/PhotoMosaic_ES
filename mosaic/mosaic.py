b8fe09bb54cb1f5a145ff3504ba4e1a32033b9af
joey3639570 <joey3639570@gmail.com>
2021-06-03 13:36:52 +0800

    Cleaning up.

--- mosaic/mosaic.py
+++ mosaic/mosaic.py
@@ -8,13 +8,13 @@ from multiprocessing import Process, Queue, cpu_count
 
 Image.MAX_IMAGE_PIXELS = None
 # Change these 3 config parameters to suit your needs...
-TILE_SIZE      = 50		# height/width of mosaic tiles in pixels
-TILE_MATCH_RES = 5		# tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT    = 8		# the mosaic image will be this many times wider and taller than the original
+TILE_SIZE      = 10		# height/width of mosaic tiles in pixels
+TILE_MATCH_RES = 10		# tile matching resolution (higher values give better fit but require more processing)
+ENLARGEMENT    = 2		# the mosaic image will be this many times wider and taller than the original
 
 TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
-OUT_FILE = 'mosaic.jpeg'
+OUT_FILE = 'mosaic.jpg'
 EOQ_VALUE = None
 
 class TileProcessor:
--- photomosaic_complete/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from .views import my_view, mosaicing, mosaicing_view
-
-urlpatterns = [
-    path('', my_view, name='my-view'),
-    path('mosaic_act/', mosaicing_view, name='mosaic_act'),
-    url(r'^mosaic', mosaicing, name='mosaic')
-]
